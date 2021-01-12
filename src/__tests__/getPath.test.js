52a7034f080f4824b9f1a8ad7a9fffa2b7cc6b33
Noopur <noopijain@gmail.com>
2021-01-12 09:23:12 +0800

    Calculate path with up to 2 line changes

--- src/__tests__/getPath.test.js
+++ src/__tests__/getPath.test.js
@@ -1,25 +1,53 @@
-import {getPath} from "../getPath";
+import { getPath } from "../getPath";
 
-describe('getPath', () => {
-    const STATIONS = {
-        'Admiralty': {'NS': 10},
-        'Aljunied': {'EW': 9},
-        'Ang Mo Kio': {'NS': 16},
-    };
+describe("getPath", () => {
+  const STATIONS = {
+    Admiralty: { NS: 10 },
+    Aljunied: { EW: 9 },
+    "Ang Mo Kio": { NS: 16 },
+    "City Hall": { NS: 25, EW: 13 },
+    Bedok: { EW: 5 },
+    Somerset: { NS: 23 },
+    "Raffles Place": { NS: 26, EW: 14 },
+    "Tanah Merah": { EW: 4, CG: 0 },
+    "Changi Airport": { CG: 2 },
+  };
 
-    it('can return empty path if origin and destination is the same', () => {
-        const sameStation = 'Admiralty';
-        expect(getPath(sameStation, sameStation, STATIONS)).toEqual([]);
-    });
+  it("can return empty path if origin and destination is the same", () => {
+    const sameStation = "Admiralty";
+    expect(getPath(sameStation, sameStation, STATIONS)).toEqual([]);
+  });
 
-    it('can throw exception if destination or origin is invalid', () => {
-        expect(() => getPath('Invalid Station', 'Invalid Station', STATIONS)).toThrowError(
-            'Invalid station, cannot calculate path'
-        );
-    });
+  it("can throw exception if destination or origin is invalid", () => {
+    expect(() =>
+      getPath("Invalid Station", "Invalid Station", STATIONS)
+    ).toThrowError("Invalid station, cannot calculate path");
+  });
 
-    it('can return a single path between two stations on the same line', () => {
-        expect(getPath('Admiralty', 'Ang Mo Kio', STATIONS))
-            .toEqual([['Admiralty', 'Ang Mo Kio']]);
-    })
+  it("can return a single path between two stations on the same line", () => {
+    expect(getPath("Admiralty", "Ang Mo Kio", STATIONS)).toEqual([
+      ["Admiralty", "Ang Mo Kio"],
+    ]);
+  });
+
+  it("can return a path between two stations on different lines with one intersection", () => {
+    expect(getPath("Bedok", "Somerset", STATIONS)).toEqual([
+      ["Bedok", "City Hall", "Somerset"],
+      ["Bedok", "Raffles Place", "Somerset"],
+    ]);
+  });
+
+  it("can return 2 possible paths", () => {
+    expect(getPath("Bedok", "Somerset", STATIONS)).toEqual([
+      ["Bedok", "City Hall", "Somerset"],
+      ["Bedok", "Raffles Place", "Somerset"],
+    ]);
+  });
+
+  it("can return a path on 2 different lines with no common interchange between them", () => {
+    expect(getPath("Ang Mo Kio", "Changi Airport", STATIONS)).toEqual([
+      ["Ang Mo Kio", "City Hall", "Tanah Merah", "Changi Airport"],
+      ["Ang Mo Kio", "Raffles Place", "Tanah Merah", "Changi Airport"],
+    ]);
+  });
 });
--- src/getPath.js
+++ src/getPath.js
@@ -1,10 +1,65 @@
+function getInterchangesBetween(station1, station2, stations) {
+  const common = Object.keys(stations[station1]).filter((line) =>
+    Object.keys(stations[station2]).includes(line)
+  );
+  if (common.length > 0) {
+    return [];
+  }
+  const interchanges = new Set();
+  Object.keys(stations[station1]).forEach((station1Line) => {
+    Object.keys(stations[station2]).forEach((station2Line) => {
+      Object.entries(stations).forEach(([station, lines]) => {
+        if (
+          Object.keys(lines).includes(station1Line) &&
+          Object.keys(lines).includes(station2Line)
+        ) {
+          interchanges.add(station);
+        }
+      });
+    });
+  });
+  return [...interchanges];
+}
+
 export function getPath(origin, destination, stations) {
-    const STATION_NAMES = Object.keys(stations);
-    if(!STATION_NAMES.includes(origin) || !STATION_NAMES.includes(destination)) {
-        throw new Error('Invalid station, cannot calculate path');
-    }
-    const path = [];
-    if(origin === destination) return path;
+  const STATION_NAMES = Object.keys(stations);
+  if (!STATION_NAMES.includes(origin) || !STATION_NAMES.includes(destination)) {
+    throw new Error("Invalid station, cannot calculate path");
+  }
+  const path = [];
+  if (origin === destination) return path;
+  const linesOrigin = stations[origin];
+  const linesDestination = stations[destination];
+  const common = Object.keys(linesOrigin).filter((line) =>
+    Object.keys(linesDestination).includes(line)
+  );
+  if (common.length > 0) {
     path.push([origin, destination]);
-    return path;
+  } else {
+    const interchanges = getInterchangesBetween(origin, destination, stations);
+    interchanges.forEach((interchange) => {
+      path.push([origin, interchange, destination]);
+    });
+    Object.keys(linesDestination).forEach((destinationLine) => {
+      Object.entries(stations).forEach(([station, line]) => {
+        if (
+          ![origin, destination].includes(station) &&
+          Object.keys(line).length > 1 &&
+          Object.keys(line).includes(destinationLine)
+        ) {
+          const interchanges = getInterchangesBetween(
+            origin,
+            station,
+            stations
+          );
+          interchanges.forEach((interchange) => {
+            if (interchange !== station) {
+              path.push([origin, interchange, station, destination]);
+            }
+          });
+        }
+      });
+    });
+  }
+  return path;
 }
